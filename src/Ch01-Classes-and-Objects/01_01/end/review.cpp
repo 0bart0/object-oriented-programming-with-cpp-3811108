05d82af1eb831984a9aacc02058d5f988789628a
Bartosz Kempa <bkempa96@gmail.com>
2025-09-06 19:24:15 +0000

    t

--- src/Ch01-Classes-and-Objects/01_01/end/review.cpp
+++ src/Ch01-Classes-and-Objects/01_01/end/review.cpp
@@ -2,12 +2,16 @@
 
 class Review
 {
+public:
+  void displayDetails() const
+  {
+    std::cout << "Rating " << rating << "/5\n";
+    std::cout << "Title: " << title << "\n";
+    std::cout << "Text: " << text << std::endl;
+  }
+
+private:
   unsigned int rating;
   std::string title;
   std::string text;
-
-  void displayDetails() const
-  {
-    std::cout << "Rating: " << rating << "/5\nTitle: " << title << "\nText: " << text << std::endl;
-  }
 };
