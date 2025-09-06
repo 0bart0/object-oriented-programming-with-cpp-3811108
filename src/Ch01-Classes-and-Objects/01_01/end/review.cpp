bfdfa70bb0407bcbb0c9ff5cd58f956c09fb6f25
Bartosz Kempa <bkempa96@gmail.com>
2025-09-06 22:59:24 +0200

    t

--- src/Ch01-Classes-and-Objects/01_01/end/review.cpp
+++ src/Ch01-Classes-and-Objects/01_01/end/review.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
+#include "review.h"
 
-class Review
+void Review::displayDetails() const
 {
-public:
-  void displayDetails() const
-  {
-    std::cout << "Rating " << rating << "/5\n";
-    std::cout << "Title: " << title << "\n";
-    std::cout << "Text: " << text << std::endl;
-  }
-
-private:
-  unsigned int rating;
-  std::string title;
-  std::string text;
-};
+  std::cout << "Rating " << rating << "/5\n";
+  std::cout << "Title: " << title << "\n";
+  std::cout << "Text: " << text << std::endl;
+}
